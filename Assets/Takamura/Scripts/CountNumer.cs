4b9c8bcb6cbc6d15b0d107bd7028188480eb909f
okazawa_R <O.riku8863@gmail.com>
2024-06-24 17:36:38 +0900

    上側にランダムに数字を表示するスクリプト

--- Assets/Takamura/Scripts/CountNumer.cs
+++ Assets/Takamura/Scripts/CountNumer.cs
@@ -1,31 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountNumber : MonoBehaviour
 {
+    [SerializeField] Text _numberText; // テキストを表示するもの
+    [SerializeField] Text _numberText1;
+    [SerializeField] Text _numberText2;
+    [SerializeField] Text _numberText3;
+    [SerializeField] Text _numberText4;
+    [SerializeField] Text _numberText5;
+    [SerializeField] Text _numberText6;
+    [SerializeField] Text _numberText7;
 
-    [SerializeField] TextMeshPro tex;
-    public int num;
+    public int num; // 現在の数字
+    public int[] _numbers; // 数字の配列
 
     void Start()
     {
-        tex = GetComponent<TextMeshPro>();
+        _numberText = GetComponent<Text>();
         //AddNumber名前変わるらしい
         AddNumber();
-        
     }
 
-    private void Update()
+    public void AddNumber() // 数字を出力する
     {
-        
-    }
+        int[] numbers = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-    private void AddNumber()
-    {
-        num = (int)Random.Range(1, 9);
-
-        Debug.Log(num);
+        System.Random random = new System.Random();
+        numbers = numbers.OrderBy(x => random.Next()).ToArray();
+        _numbers = numbers;
+        //intでほしい場合は変数から直接どうぞ
+        _numberText.text = _numbers[0].ToString();
+        _numberText1.text = _numbers[1].ToString();
+        _numberText2.text = _numbers[2].ToString();
+        _numberText3.text = _numbers[3].ToString();
+        _numberText4.text = _numbers[4].ToString();
+        _numberText5.text = _numbers[5].ToString();
+        _numberText6.text = _numbers[6].ToString();
+        _numberText7.text = _numbers[7].ToString();
     }
 }
